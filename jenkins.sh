decefb5927328777da65ed5e54ec6ad8e6c97981
Alexandru Cristu <alexandru.cristu@computaris.com>
2017-11-24 14:25:15 +0200

    adasd

--- jenkins.sh
+++ jenkins.sh
@@ -1,1 +1,5 @@
 echo "building stuff..."
+
+docker build -t docker-flask-sample .
+docker tag docker-flask-sample t01u01.azurecr.io/team01/docker-flask-sample:0.2
+docker push t01u01.azurecr.io/team01/docker-flask-sample:0.2
